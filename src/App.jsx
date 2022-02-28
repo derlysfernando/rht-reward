6ede66c33a8a85027f4e5436908a047ede05916b
Derlys <derlys.fern@gmail.com>
2022-02-28 15:45:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import GlobalStyle from "./styles/global";
 import "antd/dist/antd.css";
 import DirectSale from "./components/DirectSale";
 
-const serverUrl = "https://squniwtvf0f0.usemoralis.com:2053/server";
-const appId = "kHfsqUM7CtlORa0YeOKzAH1pBtcftNsng1TkqXtM";
+const serverUrl = "https://p0mf0i6l2azk.usemoralis.com:2053/server";
+const appId = "zEk9Ci0C9cu1fXWCC4oAD45HApMmGVqU2RHuEcOU";
 
 function App() {
   return (
